cb497ebbad040826ceec5fe6da3eff833bbf74f9
nicholay <nicholaysheremet@gmail.com>
2023-11-27 23:18:01 +0300

    react router - with misprint error

--- 01-start/src/index.js
+++ 01-start/src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import './index.css';
+import Root, { loader as rootLoader } from './components/root';
+import ErrorPage from './components/error-page';
+import Contact from './components/contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
