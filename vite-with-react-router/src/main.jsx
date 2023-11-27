fcf5a025ae7258b9f90247e2cf13e6eccb1c7a7a
nicholay <nicholaysheremet@gmail.com>
2023-11-27 23:18:13 +0300

    react router in fresh vite proj

---
+++ vite-with-react-router/src/main.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./components/root";
+import Contact, { loader as contactLoader, action as contactAction, } from "./components/contact";
+import EditContact, { action as editAction } from "./components/contact-edit";
+import { action as destroyAction } from "./components/destroy";
+import ErrorPage from "./components/error-page";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+        action: contactAction,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
