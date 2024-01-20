e24504062e46fc7dca9155cd58cffccd1c6c55d0
nicholay <nicholaysheremet@gmail.com>
2024-01-20 22:34:08 +0300

    boid templates

---
+++ styling/vite-react-redux-styled-components/src/app/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Counter } from "../components/Counter";
+import { ToDoList } from "../components/ToDoList";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React + Redux</h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <hr />
+      <h1>React-redux counter:</h1>
+      <Counter />
+      <hr />
+      <h1>React-redux ToDo List:</h1>
+      <ToDoList />
+    </>
+  );
+}
+
+export default App;
