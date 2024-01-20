0fc50f7c5e9d23bd4772810acaff26bad45c2b76
nicholay <nicholaysheremet@gmail.com>
2024-01-20 23:04:37 +0300

    + styled components

--- styling/vite-react-redux-styled-components/src/app/App.jsx
+++ styling/vite-react-redux-styled-components/src/app/App.jsx
@@ -1,24 +1,37 @@
 import { useState } from "react";
 import { Counter } from "../components/Counter";
 import { ToDoList } from "../components/ToDoList";
+import styled from "styled-components";
 
 function App() {
   const [count, setCount] = useState(0);
+  const Wrapper = styled.div`
+    padding: 3rem;
+  `;
+
+  const Title = styled.h1`
+    font-size: 1.5em;
+    color: #bf4f74;
+  `;
 
   return (
     <>
-      <h1>Vite + React + Redux</h1>
-      <div>
+      <Wrapper>
+        <Title>Vite + React + Redux</Title>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>
+      </Wrapper>
       <hr />
-      <h1>React-redux counter:</h1>
-      <Counter />
+      <Wrapper>
+        <Title>React-redux counter:</Title>
+        <Counter />
+      </Wrapper>
       <hr />
-      <h1>React-redux ToDo List:</h1>
-      <ToDoList />
+      <Wrapper>
+        <Title>React-redux ToDo List:</Title>
+        <ToDoList />
+      </Wrapper>
     </>
   );
 }
